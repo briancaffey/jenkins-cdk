540f5b499ce80eab2b79d0e04b226688f6550d93
Brian Caffey <briancaffey2010@gmail.com>
2021-10-20 20:34:51 -0400

    chore(deps): update cdk version

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const { AwsCdkConstructLibrary } = require('projen');
 const project = new AwsCdkConstructLibrary({
   author: 'Brian Caffey',
   authorAddress: 'briancaffey2010@gmail.com',
-  cdkVersion: '1.110.1',
+  cdkVersion: '1.128.0',
   defaultReleaseBranch: 'main',
   name: 'jenkins-cdk',
   repositoryUrl: 'https://github.com/briancaffey2010/jenkins-cdk.git',
